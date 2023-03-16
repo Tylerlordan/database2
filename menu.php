6b050b5d1bdeee3a9e9520dbcc62b4e900469a61
Tylerlordan <71417203+Tylerlordan@users.noreply.github.com>
2023-03-16 11:15:47 -0400

    Small changes to menu

--- menu.php
+++ menu.php
@@ -1,22 +1,220 @@
 <?php
+// Function for generating the html elements found in the main menu, 
+// changing the displayed elements based on the type of user that is signed in.
 function create_menu($id, $usertype) {
     if($usertype === "student") {
         echo '<form action="update_account.php" method="post">
         <table border="0">     
         <tr bgcolor="#cccccc">
-        <div name="user_id" value ="'.$id.'"></div>
-          <td colspan="2" align="center"><input type="submit" value="Update Account"/></td>
+        <td align="center"><h2>Update Account</h2></td>
+        <tr bgcolor="#aaaaff">
+            <td align="left"><p>Empty values will be left unchanged.</p></td>
+        </tr>
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <tr bgcolor="#cccccc">
+            <td>New Name:</td>
+            <td align="left"><input type="text" name="new_name" size="30" maxlength="30" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Email:</td>
+            <td align="left"><input type="text" name="new_email" size="30" maxlength="100" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Phone:</td>
+            <td align="left"><input type="text" name="new_phone" size="30" maxlength="10" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Password:</td>
+            <td align="left"><input type="password" name="new_password" size="30" maxlength="20" required/></td>
+        </tr>
+        <td colspan="2" align="center"><input type="submit" value="Update Account"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="view_groups_meetings.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="View Groups and Meetings"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="join_meeting.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Join a Meeting"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="leave_meeting.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Leave a Meeting or All Meetings"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="view_meeting_members.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="View Groups and Meetings"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="view_materials.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="View Materials"/></td>
         </tr>
         </table>
         </form>';
     }
     elseif($usertype === "parent") {
-        echo file_get_contents("studentpage.html");
-        echo "<div name='user_id' value ='".$id."'></div>";
+        echo '<form action="update_account.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <td align="center"><h2>Update Account</h2></td>
+        <tr bgcolor="#aaaaff">
+            <td align="left"><p>Empty values will be left unchanged.</p></td>
+        </tr>
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <tr bgcolor="#cccccc">
+            <td>Student email(optional*):</td>
+            <td align="left"><input type="text" name="student" size="30" maxlength="100" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Name:</td>
+            <td align="left"><input type="text" name="new_name" size="30" maxlength="30" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Email:</td>
+            <td align="left"><input type="text" name="new_email" size="30" maxlength="100" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Phone:</td>
+            <td align="left"><input type="text" name="new_phone" size="30" maxlength="10" required/></td>
+        </tr>
+        <tr bgcolor="#cccccc">
+            <td>New Password:</td>
+            <td align="left"><input type="password" name="new_password" size="30" maxlength="20" required/></td>
+        </tr>
+        <td align="left"><p>*Enter your child\'s email if editing their account,</p></td>
+        <tr></tr>
+        <td align="left"><p>Leave empty to edit your own account</p></td>
+        <td colspan="2" align="center"><input type="submit" value="Update Account"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="view_groups_meetings.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="View Groups and Meetings"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="leave_child_meeting.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Remove a Child from a Meeting"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="claim_child.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Claim Child Account"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
     }
     elseif($usertype === "admin") {
-        echo file_get_contents("parentpage.html");
-        echo "<div name='user_id' value ='".$id."'></div>";
+        echo '<form action="mod_meetings.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Modify Meeting Details"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="mod_materials.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Modify Materials Details"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="view_groups_meetings.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="View Groups and Meetings"/></td>
+        </tr>
+        </table>
+        </form>';
+
+        echo "<br><br><br>";
+
+        echo '<form action="post_materials.php" method="post">
+        <table border="0">     
+        <tr bgcolor="#cccccc">
+        <div name="user_id" value ="'.$id.'" hidden></div>
+        <div name="user_type" value ="'.$usertype.'" hidden></div>
+        <td colspan="2" align="center"><input type="submit" value="Post Study Materials"/></td>
+        </tr>
+        </table>
+        </form>';
     }
 }
 ?>
